93d4a69092b162ab2afa7ea31a832ec09ea14b10
Elías <elias.ludviksson@gmail.com>
2023-11-03 18:55:37 +0000

    Kvittun komin en nellifly med ves

--- main.js
+++ main.js
@@ -114,11 +114,54 @@ sendaInn.addEventListener('submit', klara);
 
 function klara(event) {
   event.preventDefault()
-
+  const nafn = event.target[0].value
+  const heimili = event.target[1].value
+  showreceipt(nafn,heimili);
 }
 
-function showreceipt() {
+function showreceipt(nafn,heimili) {
+  const dot = document.querySelectorAll('.cart table tbody tr');
+  // sýnum kvittun og felum rest
+  const pro = document.querySelector('.products');
+  const car = document.querySelector('.cart');
+  const rec = document.querySelector('.receipt');
+  if (!pro || !car || !rec) {
+    console.warn('fann ekki element.');
+    return;
+  }
+  const kvittun = document.createElement('ul');
+  pro.classList.add('hidden');
+  car.classList.add('hidden');
+  rec.classList.remove('hidden');
+  // 
+  const newlnafn = document.createElement('li');
+  kvittun.appendChild(newlnafn);
+  const setning = document.createElement('p');
+  setning.textContent = 'Pöntun móttekin: ' + String(nafn) + ' Heimilisfang: ' + String(heimili);
+  newlnafn.appendChild(setning); 
+  // tæmum cart
+  let finalprice = 0;
+  for (const efni of Array.from(dot)) {
+    const newl = document.createElement('li');
+    kvittun.appendChild(newl);
+    const magn = efni.querySelector('.foo').innerHTML;
+    const product = products.find((i) => i.id === Number(efni.dataset.productId));
+    efni.parentElement.removeChild(efni);
+    const setning = document.createElement('p');
+    setning.textContent = String(product.title) + ': ' + String(formatPrice(product.price)) + ' ' + String(magn) + ' stykki'
+    newl.appendChild(setning); 
+    finalprice	+= parseFloat(product.price)*Number(magn);
+  }
+  const newl = document.createElement('li');
+  newl.textContent = 'Samtals: ' + formatPrice(finalprice);
+  kvittun.appendChild(newl);
+  resettotal()
 
-
-
+  const takk = document.createElement('p');
+  takk.textContent = 'Takk fyrir að versla hjá okkur!'
+  const meira = document.createElement('a')
+  meira.textContent = 'Kaupa meira.'
+  meira.setAttribute('href', '.')
+  
+  rec.append(kvittun,takk,meira)
 }
